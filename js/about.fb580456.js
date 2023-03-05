2f40f856ebae4c5b38b33b8ab692e856b38cccab
NiBog <nokaut05@gmail.com>
2023-03-05 18:56:59 -0500

    New Deployment

---
+++ js/about.fb580456.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkkk_wedding"]=self["webpackChunkkk_wedding"]||[]).push([[443],{135:function(n,t,u){u.r(t),u.d(t,{default:function(){return r}});var e=function(){var n=this;n._self._c;return n._m(0)},s=[function(){var n=this,t=n._self._c;return t("div",{staticClass:"about"},[t("h1",[n._v("This is an about page")])])}],i=u(736),a={},l=(0,i.Z)(a,e,s,!1,null,null,null),r=l.exports}}]);
+//# sourceMappingURL=about.fb580456.js.map
